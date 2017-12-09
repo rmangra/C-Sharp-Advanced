70fcc16d098c0dcaec55c4753ea2977ab8e04e96
rmangra <richard.mangra@gmail.com>
2017-12-09 14:14:43 -0500

    Example showing the use of Generics to avoid code duplication, and reduce performance penalty of using an object list.

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new Nullable<int>(42);
+            Console.WriteLine("Has Value ?" + number.HasValue);
+            Console.WriteLine("Value: " + number.GetValueOrDefault());
+        }
+    }
+}
