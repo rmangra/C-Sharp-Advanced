3d8c9cd694dc15dc5656ff2d2b4c45a745928207
rmangra <richard.mangra@gmail.com>
2017-12-11 16:00:16 -0500

    Started Lambda Expressions lecture.

---
+++ LambdaExpressions1/LambdaExpressions1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lambda expression syntax:
+            // args => expression
+            number => number * number;
+
+            Console.WriteLine(Square(5));
+        }
+
+        static int Square(int number)
+        {
+            return number * number;
+        }
+    }
+}
