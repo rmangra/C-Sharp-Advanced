46d4a6276c4b67216d9e4f46faac937d76b46cd3
rmangra <richard.mangra@gmail.com>
2017-12-11 15:21:39 -0500

    Added a PhotoProcessor class.

--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace Delegates
 {
+    public delegate void PhotoFilterHandler(Photo photo);
+
     class Program
     {
         static void Main(string[] args)
         {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
 
+            processor.Process("photo.jpg", filterHandler);
         }
+    }
     }
 }
