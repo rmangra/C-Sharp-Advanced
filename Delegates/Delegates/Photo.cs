2becb31f1561c03e896c7a240eedbf21f728c39e
rmangra <richard.mangra@gmail.com>
2017-12-11 15:24:26 -0500

    Added a Photo class.

---
+++ Delegates/Delegates/Photo.cs
@@ -0,0 +1,19 @@
+﻿namespace Delegates
+{
+    partial class Program
+    {
+        public class Photo
+        {
+            public static Photo Load(string path)
+            {
+                return new Photo();
+            }
+
+            public void Save()
+            {
+
+            }
+        }
+    }
+    }
+}
