5d8a6a86f1f2dbe8ae3afb8e79b377fade71c42d
rmangra <richard.mangra@gmail.com>
2017-12-12 18:12:46 -0500

    Created a VideoEncoder class.

---
+++ EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsAndDelegates
+{
+    public class VideoEncoder
+    {
+        // 1) Define a delegate
+        // 2) Define an event based on that delegate
+        // 3) Raise the event
+
+        public delegate
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+        }
+    }
+}
