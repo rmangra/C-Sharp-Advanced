b75b36f2cd056c9008f0c26a229faabb75e6d1d1
rmangra <richard.mangra@gmail.com>
2017-12-12 17:56:09 -0500

    Created book repository class.

--- LambdaExpressions2/LambdaExpressions2/Program.cs
+++ LambdaExpressions2/LambdaExpressions2/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +9,15 @@ namespace LambdaExpressions2
     {
         static void Main(string[] args)
         {
+            var books = new BookRepository().GetBooks();
+
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
         }
     }
 }
