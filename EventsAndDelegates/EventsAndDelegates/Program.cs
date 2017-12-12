827e49f174d971b7875998ed197993ecd1f08c93
rmangra <richard.mangra@gmail.com>
2017-12-12 18:38:10 -0500

    Created a MailService class.

--- EventsAndDelegates/EventsAndDelegates/Program.cs
+++ EventsAndDelegates/EventsAndDelegates/Program.cs
@@ -10,6 +10,13 @@ namespace EventsAndDelegates
     {
         static void Main(string[] args)
         {
+            var video = new Video() {Title = "Video 1"};
+            var videoEncoder = new VideoEncoder(); // publisher
+            var mailService = new MailService();  // subscriber
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
         }
     }
 }
