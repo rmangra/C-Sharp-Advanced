dc10b943ca5595dc3be7979c61bce726a69556f8
rmangra <richard.mangra@gmail.com>
2017-12-12 18:24:48 -0500

    Defined an event based on the delegate.

--- EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
+++ EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
@@ -11,10 +11,20 @@ namespace EventsAndDelegates
 
         public delegate void VideoEncodedEventHandler(object source, EventArgs args);
 
+        public event VideoEncodedEventHandler VideoEncoded;
+
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding Video...");
             Thread.Sleep(3000);
+
+            OnVideoEncoded();
+        }
+
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, EventArgs.Empty);
         }
     }
 }
