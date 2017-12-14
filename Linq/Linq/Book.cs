631c4178d877755187e19445215cceb18b39dcce
rmangra <richard.mangra@gmail.com>
2017-12-13 22:00:29 -0500

    Added BookRepository class.

--- Linq/Linq/Book.cs
+++ Linq/Linq/Book.cs
@@ -1,11 +1,9 @@
 ﻿namespace Linq
 {
-    partial class Program
-    {
-        public class Book
-        {
-            public string Title { get; set; }
-            public float Price { get; set; }
-        }
+    public class Book
+    {    
+        public string Title { get; set; }
+        public float Price { get; set; }
+      
     }
 }
--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Linq
 {
@@ -11,8 +8,29 @@ namespace Linq
 
         static void Main(string[] args)
         {
-            Func<int, int, int> add = (a, b) => a + b;
-            Console.WriteLine(add(2, 3));
+            var books = new BookRepository().GetBooks();
+
+            // LINQ Query Operators
+            var cheaperBooks =
+                from b in books
+                where b.Price < 10
+                orderby b.Title
+                select b.Title;
+
+            // LINQ Extension Methods
+            var cheapBooks = books
+                .Where(b => b.Price < 10)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
+
+            foreach (var book in cheapBooks)
+                Console.WriteLine(book);
+            
+            
+            // More LINQ Extension Methods
+            //books.OrderBy(b => b.Title)
+            //Func<int, int, int> add = (a, b) => a + b;
+            //Console.WriteLine(add(2, 3));
         }
 
         private static float CalculateDiscount(float price)
