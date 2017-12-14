7ff0b0743d8467a5d41cd0f0e0a52f8059b25082
rmangra <richard.mangra@gmail.com>
2017-12-13 21:45:45 -0500

    Added Book class.

--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace Linq
 {
-    class Program
+    partial class Program
     {
+
         static void Main(string[] args)
         {
+            Func<int, int, int> add = (a, b) => a + b;
+            Console.WriteLine(add(2, 3));
+        }
+
+        private static float CalculateDiscount(float price)
+        {
+            return 0;
         }
     }
 }
