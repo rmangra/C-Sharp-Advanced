918b88e0eb717ae66ff852b216a0374420d5115b
rmangra <richard.mangra@gmail.com>
2017-12-14 22:16:57 -0500

    Completed Dynamic type lecture examples.

--- DynamicBinding/DynamicBinding/Program.cs
+++ DynamicBinding/DynamicBinding/Program.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DynamicBinding
+﻿namespace DynamicBinding
 {
     class Program
     {
         static void Main(string[] args)
         {
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b;
 
+            int i = 5;
+            dynamic d = i;
+            long l = d;
         }
     }
 }
