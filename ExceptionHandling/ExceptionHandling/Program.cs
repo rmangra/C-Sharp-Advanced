3aada88a8d2f7e79b5ecbf58896f18d85c87567a
rmangra <richard.mangra@gmail.com>
2017-12-14 22:20:50 -0500

    Started working on Exception handling lecture examples.

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var api = new YouTubeApi();
+                var videos = api.GetVideos("Richard");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
